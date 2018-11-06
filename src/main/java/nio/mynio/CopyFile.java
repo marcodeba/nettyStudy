1bb3b6144055721560c9315dfcbe3525edade733
panning255 <panning255@jk.cn>
2018-11-07 00:08:31 +0800

    add nio.mynio

---
+++ src/main/java/nio/mynio/CopyFile.java
@@ -0,0 +1,35 @@
+package nio.mynio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class CopyFile {
+	public static void main(String[] args) throws Exception {
+		String infile = "CopyFile.java";
+		String outfile = "CopyFile.txt";
+		
+		FileInputStream fin = new FileInputStream(infile);
+		FileOutputStream fout = new FileOutputStream(outfile);
+		
+		FileChannel fcin = fin.getChannel();
+		FileChannel fcout = fout.getChannel();
+		
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		
+		int bytesRead = fcin.read(buffer);
+		while (-1 != bytesRead) {
+			buffer.flip();
+			while (buffer.hasRemaining()) {
+				fcout.write(buffer);
+			}
+  			buffer.clear();
+  			bytesRead = fcin.read(buffer);
+		}
+		fcin.close();
+		fcout.close();
+		fin.close();
+		fout.close();
+	}	
+}
